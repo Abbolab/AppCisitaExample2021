ca1fdd18e19e507eb27e5cb831f64ab58f432553
Francesco Abbo <francesco.abbo@xonne.it>
2021-04-29 19:26:42 +0200

    First commit to share source code

---
+++ app/src/main/java/it/cisita/appcisita2021/MainActivity.java
@@ -0,0 +1,41 @@
+package it.cisita.appcisita2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // setting di un file di layout custom
+        setContentView(R.layout.main_layout);
+
+        // refenzio programmaticamente la TextView presente nel layout
+        TextView myTextView = findViewById(R.id.textViewTest);
+        myTextView.setText("Pulsante premuto: 0 volte");
+
+        //refenzio pulsante
+        Button myButton = findViewById(R.id.buttonTest);
+        // implementando Listener per intercettare OnClick pulsante
+        myButton.setOnClickListener(new View.OnClickListener() {
+
+            int count = 0;
+
+            @Override
+            public void onClick(View view) {
+                count++;
+                myTextView.setText("Pulsante premuto: " + String.valueOf(count) + " volte");
+                Log.i("CISITA", "Pulsante premuto!!!");
+            }
+        });
+
+    }
+}
